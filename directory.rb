bd916cbafba6b57dd4edcb922ed7b1e9e71f0d2c
ruby4life <rubyapp.master@gmail.com>
2016-05-22 23:25:06 +0100

    added students.csv to save student names

--- directory.rb
+++ directory.rb
@@ -16,8 +16,9 @@ def print_footer
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To commit names, just hit return twice"
+  puts "Please enter the name of the student"
+  puts "To Save the name of the student press enter once"
+  puts "To Exit, hit return twice"
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -33,7 +34,8 @@ end
 def print_menu    
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "5. Exit" # 9 because we'll be adding more items  
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
 end
 
 def show_students
@@ -42,13 +44,36 @@ def show_students
   print_footer
 end
 
+
+
+
+
+def save_students
+  file = File.open("students.csv","w")
+
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+  
+
 def process(selection)
   case selection
+
   when "1"
     input_students
+
   when "2"
     show_students
-  when "5"
+
+  when "3"
+   save_students
+   print "The names listed have been saved"
+
+  when "9"
     exit
   else
     puts "I don't know what you mean, try again"
