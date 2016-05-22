cf0b6953b6ddb7f18231854f2acf520355d9e090
ruby4life <rubyapp.master@gmail.com>
2016-05-22 23:02:29 +0100

    students now have {:name => Foo, :cohort => :augest} and the print(students) method puts '#{student[:name]} (#{student[:cohort]} cohort)'

--- directory.rb
+++ directory.rb
@@ -1,41 +1,43 @@
 
 students = [
-"Faisal Aydarus",
-"mayadrivermd",
-"michballard",
-"henry stanley", 
-"jerome.pratt",
-"chris oatley",
-"ilccyk",
-"leo poldkwok",
-"tillett",
-"nickrhys",
-"james.r.mcneil", 
-"lovis-schultze",
-"tatienantes",
-"iblockchain",
-"v.koch",
-"byverdu", 
-"dave wickes",
-"silver io funk", 
-"andy gates010", 
-"spike01",
-"global avocado", 
- "mr. yogesh"
+{:name => "Faisal Aydarus", :cohort => :august},
+{:name => "mayadrivermd", :cohort => :august},
+{:name => "michballard", :cohort => :august},
+{:name => "henry stanley", :cohort => :august}, 
+{:name => "jerome.pratt", :cohort => :august},
+{:name => "chris oatley", :cohort => :august},
+{:name => "ilccyk", :cohort => :august},
+{:name => "leo poldkwok", :cohort => :august},
+{:name => "tillett", :cohort => :august},
+{:name => "nickrhys", :cohort => :august},
+{:name => "james.r.mcneil", :cohort => :august}, 
+{:name => "lovis-schultze", :cohort => :august},
+{:name => "tatienantes", :cohort => :august},
+{:name => "iblockchain", :cohort => :august},
+{:name => "v.koch", :cohort => :august},
+{:name => "byverdu", :cohort => :august}, 
+{:name => "dave wickes", :cohort => :august},
+{:name => "silver io funk", :cohort => :august}, 
+{:name => "andy gates010", :cohort => :august}, 
+{:name => "spike01", :cohort => :august},
+{:name => "global avocado", :cohort => :august}, 
+{:name => "mr. yogesh", :cohort => :august}
 ]
+
 def print_header
 puts "The students of my cohort at Makers Academy"
 puts "___________________________________________"
 end
 
-def print(names)
-	names.each do |name|
-		puts name
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
 def print_footer(names)	
-	print "Overall, we have #{students.length} great students "
+
+	puts "Overall, we have #{names.length} great students "
 end
 
 
