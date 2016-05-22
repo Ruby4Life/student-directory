e91d6b200bac884df107245b29a0279b50510d60
ruby4life <rubyapp.master@gmail.com>
2016-05-22 22:46:11 +0100

    added list of students to a [students] array and added a for each loop to display each student in the array

--- directory.rb
+++ directory.rb
@@ -1,29 +1,32 @@
-student_count = 22
-
 puts "The students of my cohort at Makers Academy"
-puts "test1"
-puts "test2 test"
-puts "Faisal Aydarus"
-puts "mayadrivermd"
-puts "michballard"
-puts "henry stanley" 
-puts "jerome.pratt" 
-puts "chris oatley"
-puts "ilccyk"
-puts "leo poldkwok"
-puts "tillett"
-puts "nickrhys"
-puts "james.r.mcneil" 
-puts "lovis-schultze"
-puts "tatienantes"
-puts "iblockchain" 
-puts "v.koch"
-puts "byverdu" 
-puts "dave wickes"
-puts "silver io funk" 
-puts "andy gates010" 
-puts "spike01"
-puts "global avocado" 
-puts "mr. yogesh"
+students = [
+"Faisal Aydarus",
+"mayadrivermd",
+"michballard",
+"henry stanley", 
+"jerome.pratt",
+"chris oatley",
+"ilccyk",
+"leo poldkwok",
+"tillett",
+"nickrhys",
+"james.r.mcneil", 
+"lovis-schultze",
+"tatienantes",
+"iblockchain",
+"v.koch",
+"byverdu", 
+"dave wickes",
+"silver io funk", 
+"andy gates010", 
+"spike01",
+"global avocado", 
+ "mr. yogesh"
+]
 
-print "Overall, we have #{student_count} great students "
+students.each do | student |
+
+
+puts student
+end
+print "Overall, we have #{student_length} great students "
